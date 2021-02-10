323339f9f492f7048b5cc80134bd4d96c0bc882a
KARL NASSAR <kgnassar01@gmail.com>
2021-02-10 21:28:33 +0200

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,11 +14,11 @@ const About = () => {
                 <div className="about__paragraph">
                     <h1>About Me</h1>
                     <p>
-                        Hi There! I am Karl! I've been coding for 3 months. Currently studyin at the Lebanese University, I am a 
+                        Hi There! I am Karl! I've been coding for 3 months. Currently studying at the Lebanese University, I am a 
                         passionate learner about coding and full stack development. My journey started when I came across a youtube 
                         video talking about react.js and how it is becoming the best web framework in 2020; this got me instantly
-                        hooked to javascript and web development. Always seeking for new challenges and adventures, I hope to take 
-                        my coding skills and problem solving to the next level.
+                        hooked to javascript and web development. Always seeking new challenges and adventures, I hope to take 
+                        my coding and problem solving skills to the next level.
                     </p>
                 </div>
 
