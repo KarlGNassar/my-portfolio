a6f530641e39fd2dace8975b25a643c65f994a37
KARL NASSAR <kgnassar01@gmail.com>
2021-05-13 00:56:21 +0300

    fixed typos

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,64 +1,85 @@
-import React from 'react'
-import './Contact.css'
-import emailjs from 'emailjs-com'
-import { HiMail } from 'react-icons/hi'
+import React from 'react';
+import './Contact.css';
+import emailjs from 'emailjs-com';
+import { HiMail } from 'react-icons/hi';
 
 const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        emailjs.sendForm('service_sbiey5i', 'template_vhm3lfc', e.target, 'user_tSO4zJrwJzQkF1DpUZCGg')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-        e.target.reset()
-    }
-    return (
-        <div className="contact" id="contactId">
-            <div className="contact__header">
-                <div className="contact__icon">
-                    <HiMail />
-                </div>
-                <div className="contact__title">
-                    <h1>Get in touch with me.</h1>
-                </div>
+    emailjs
+      .sendForm(
+        'service_sbiey5i',
+        'template_vhm3lfc',
+        e.target,
+        'user_tSO4zJrwJzQkF1DpUZCGg',
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
+    e.target.reset();
+  };
+  return (
+    <div className="contact" id="contactId">
+      <div className="contact__header">
+        <div className="contact__icon">
+          <HiMail />
+        </div>
+        <div className="contact__title">
+          <h1>Get in touch with me.</h1>
+        </div>
+      </div>
+      <div className="contact__container">
+        <div className="contact__form">
+          <form onSubmit={sendEmail}>
+            <div className="contact__input">
+              <p>
+                Name <span className="contact__required">*</span>
+              </p>
+              <input type="text" placeholder="Name" name="name" required />
             </div>
-            <div className="contact__container">
-                <div className="contact__form">
-                    <form onSubmit={sendEmail}>
-                        <div className="contact__input">
-                        <p>Name <span className="contact__required">*</span></p>
-                        <input type="text" placeholder="Name" name="name" required/>
-                        </div>
-                        <div className="contact__input">
-                            <p>Email <span className="contact__required">*</span></p>
-                            <input type="email" placeholder="Email" name="name" required/>
-                        </div>
-                        <div className="contact__input">
-                            <p>Subject</p>
-                            <input type="text" placeholder="Subject" name="subject"/>
-                        </div>
-                        <div className="contact__input">
-                            <p>Message <span className="contact__required">*</span></p>
-                            <textarea name="message" id="" cols="30" rows="10" placeholder="Message" required></textarea>
-                        </div>
-                        <input type="submit" value="SUBMIT" className="contact__button"/>
-                    </form>
-                </div>
-                <div className="contact__address">
-                    <h2>Address On Phone</h2>
-                    <p>Karl Nassar</p>
-                    <p>kgnassar01@gmail.com</p>
-                    <p>Zalka, Beirut</p>
-                    <p>Moun Lebanon, Lebanont</p>
-                    <p>+961 70 888 558</p>
-                </div>
+            <div className="contact__input">
+              <p>
+                Email <span className="contact__required">*</span>
+              </p>
+              <input type="email" placeholder="Email" name="name" required />
             </div>
-        </div> 
-    )
-}
+            <div className="contact__input">
+              <p>Subject</p>
+              <input type="text" placeholder="Subject" name="subject" />
+            </div>
+            <div className="contact__input">
+              <p>
+                Message <span className="contact__required">*</span>
+              </p>
+              <textarea
+                name="message"
+                id=""
+                cols="30"
+                rows="10"
+                placeholder="Message"
+                required
+              ></textarea>
+            </div>
+            <input type="submit" value="SUBMIT" className="contact__button" />
+          </form>
+        </div>
+        <div className="contact__address">
+          <h2>Address On Phone</h2>
+          <p>Karl Nassar</p>
+          <p>kgnassar01@gmail.com</p>
+          <p>Zalka, Beirut</p>
+          <p>Moun Lebanon, Lebanon</p>
+          <p>+961 70 888 558</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,102 +1,93 @@
-import React from 'react'
-import './Resume.css'
-import{ FaDatabase } from 'react-icons/fa'
-import { GoStar } from 'react-icons/go'
-import { FiMonitor } from 'react-icons/fi'
+import React from 'react';
+import './Resume.css';
+import { FaDatabase } from 'react-icons/fa';
+import { GoStar } from 'react-icons/go';
+import { FiMonitor } from 'react-icons/fi';
 
 const Resume = () => {
-    return (
-        <div id="resumeId" className="resume">
-                <div className="resume__option">
-                        <div className="resume__optionLeft">
-                            <h1 className="resume__title education">Education</h1>
-                        </div>
+  return (
+    <div id="resumeId" className="resume">
+      <div className="resume__option">
+        <div className="resume__optionLeft">
+          <h1 className="resume__title education">Education</h1>
+        </div>
 
-                        <div className="resume__optionRight education">
-                            <div className="resume__info">
-                                    <h1>
-                                        LU - Faculty of Engineer - Branch II
-                                    </h1>
+        <div className="resume__optionRight education">
+          <div className="resume__info">
+            <h1>LU - Faculty of Engineer - Branch II</h1>
 
-                                    <h4>
-                                        Bachelor's Degree, Software Engineer.
-                                    </h4>
+            <h4>Bachelor's Degree, Software Engineer.</h4>
 
-                                    <p>
-                                        Currently a second year student.
-                                    </p>
-                            </div>
-                        </div>
-                </div>
-                <div className="resume__option">
-                    <div className="resume__optionLeft">
-                        <h1 className="resume__title work">Work</h1>
-                    </div>
+            <p>Currently a second year student.</p>
+          </div>
+        </div>
+      </div>
+      <div className="resume__option">
+        <div className="resume__optionLeft">
+          <h1 className="resume__title work">Work</h1>
+        </div>
 
-                    <div className="resume__optionRight work">
-                        <div className="resume__info">
-                                <h1>
-                                    No work experience yet.
-                                </h1>
+        <div className="resume__optionRight work">
+          <div className="resume__info">
+            <h1>No work experience yet.</h1>
 
-                                <h4>
-                                    Joined many communities on Discord and Reddit.
-                                </h4>
+            <h4>Backend Developer At Above the Fold</h4>
 
-                                <p>
-                                    Passionate for a chance to dive deep into a professional workspace.
-                                </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="resume__option">
-                    <div className="resume__optionLeft">
-                        <h1 className="resume__title skills">Skills</h1>
-                    </div>
+            <p>
+              Learned new techs and worked on a utility app for a private
+              company. Also implemented a private chat on a side project.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="resume__option">
+        <div className="resume__optionLeft">
+          <h1 className="resume__title skills">Skills</h1>
+        </div>
 
-                    <div className="resume__optionRight skills">
-                        <div className="resume__info">
-                            <div className="resume__wrapper">
-                                <div className="resume__infoSkills">
-                                    <FiMonitor className="resume__icon" />
-                                    <h1>Frontend</h1>
-                                    <p>ReactJS</p>
-                                    <p>HTML5</p>
-                                    <p>CSS3</p>
-                                    <p>JavaScript</p>
-                                    <p>Bootstrap</p>
-                                    <p>Material UI</p>
-                                </div>
+        <div className="resume__optionRight skills">
+          <div className="resume__info">
+            <div className="resume__wrapper">
+              <div className="resume__infoSkills">
+                <FiMonitor className="resume__icon" />
+                <h1>Frontend</h1>
+                <p>ReactJS</p>
+                <p>HTML5</p>
+                <p>CSS3</p>
+                <p>JavaScript</p>
+                <p>Bootstrap</p>
+                <p>Material UI</p>
+              </div>
 
-                                <div className="resume__infoSkills">
-                                    <FaDatabase className="resume__icon"/>
-                                    <h1>Backend</h1>
-                                    <p>NodeJS</p>
-                                    <p>NPM</p>
-                                    <p>Expres</p>
-                                    <p>SQL</p>
-                                    <p>MongoDB</p>
-                                    <p>Firebase</p>
-                                    <p>Rest API</p>
-                                    <p>Python</p>
-                                </div>
+              <div className="resume__infoSkills">
+                <FaDatabase className="resume__icon" />
+                <h1>Backend</h1>
+                <p>NodeJS</p>
+                <p>NPM</p>
+                <p>Express</p>
+                <p>SQL</p>
+                <p>MongoDB</p>
+                <p>Firebase</p>
+                <p>Rest API</p>
+                <p>Python</p>
+              </div>
 
-                                <div className="resume__infoSkills">
-                                    <GoStar className="resume__icon" />
-                                    <h1>Others</h1>
-                                    <p>GitHub</p>
-                                    <p>Redux</p>
-                                    <p>GraphQL</p>
-                                    <p>Yarn</p>
-                                    <p>C++</p>
-                                    <p>VS Code</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <div className="resume__infoSkills">
+                <GoStar className="resume__icon" />
+                <h1>Others</h1>
+                <p>GitHub</p>
+                <p>Redux</p>
+                <p>GraphQL</p>
+                <p>Yarn</p>
+                <p>C++</p>
+                <p>VS Code</p>
+              </div>
             </div>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Resume
+export default Resume;
